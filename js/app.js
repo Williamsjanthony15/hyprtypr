ba17f11b642cec5bd7cb6d8fc14743d52d3a4132
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-03-03 23:26:49 -0600

    Started JavaScript, Created Word Array, Initial Function beginning, moving towards Local Storage.

--- js/app.js
+++ js/app.js
@@ -2,4 +2,22 @@
 
 console.log('WWLND');
 
+// --- TODO  ---
+// Username 
+// Local Storage 
+// Game Logic  ---- 
+// Word Generator - Array (Hard Coded words) // DONE
+// Appending Words to table
+// Hangman Generator (pictures)
+// Scoring method
+// stretch goal -- Adding nested for loops seperate arrays
 
+
+Let wordList = ['mask', 'pizza', 'covid', 'pliers', 'camera', 'vacuum',
+  'pizazz', 'library', 'channel', 'vaccine', 'suburban', 'cemetery', 'calendar', 'separate', 'misspell', 'argument', 'assuming', 'definite', 'positive', 'negative', 'dachshund', 'necessary', 'possession', 'supposedly', 'quarantine', 'obstinance', 'millennium', 'processing', 'sovereignty', 'accommodate', 'fluorescent', 'mischievous', 'accidentally', 'questionnaire', 'pronunciation', 'capitalization']
+
+function Data(name) {
+  this.name = name; 
+}
+
+let getData = localStorage.getItem('Data');
