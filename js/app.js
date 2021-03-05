434229f496f4224c5b799c45411f425e9219a481
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-03-04 21:56:07 -0600

    initial variables, countdown timer started

--- js/app.js
+++ js/app.js
@@ -11,13 +11,29 @@ console.log('WWLND');
 // Hangman Generator (pictures)
 // Scoring method
 // stretch goal -- Adding nested for loops seperate arrays
+let temp = document.querySelector('.time');
+let button = document.querySelector('button');
+let timerDiv = document.querySelector('.time');
+let scoreDiv = document.querySelector('.score');
+let points = 0;
+let seconds = 10;
+let totalGuess = 0;
+let wrongGuess= 10;
+// button ID - play
 
+const wordList = [ 'mask', 'pizza', 'covid', 'pliers', 'camera', 'vacuum',
+  'pizazz', 'library', 'channel', 'vaccine', 'suburban', 'cemetery', 'calendar', 'separate', 'misspell', 'argument', 'assuming', 'definite', 'positive', 'negative', 'dachshund', 'necessary', 'possession', 'supposedly', 'quarantine', 'obstinance', 'millennium', 'processing', 'sovereignty', 'accommodate', 'fluorescent', 'mischievous', 'accidentally', 'questionnaire', 'pronunciation', 'capitalization' ];
+console.log(wordList);
 
-Let wordList = ['mask', 'pizza', 'covid', 'pliers', 'camera', 'vacuum',
-  'pizazz', 'library', 'channel', 'vaccine', 'suburban', 'cemetery', 'calendar', 'separate', 'misspell', 'argument', 'assuming', 'definite', 'positive', 'negative', 'dachshund', 'necessary', 'possession', 'supposedly', 'quarantine', 'obstinance', 'millennium', 'processing', 'sovereignty', 'accommodate', 'fluorescent', 'mischievous', 'accidentally', 'questionnaire', 'pronunciation', 'capitalization']
-
-function Data(name) {
-  this.name = name; 
+function countdown() {
+  points = 0;
+  let timer = setInterval(function() {
+    button.disabled = true;
+    seconds --;
+    temp.gameHtml = seconds;
+    if (seconds === 0) {
+      alert ('Game Over! You score is' + {points});
+    }
+  }
+  );
 }
-
-let getData = localStorage.getItem('Data');
