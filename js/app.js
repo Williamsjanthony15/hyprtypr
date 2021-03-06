ea93a6580c88e92e9aa8cd6af99ede96db940ee9
Dion Johnson <dionjwa@gmail.com>
2021-03-06 10:15:57 -0800

    added text box in js and game html

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@
 console.log('WWLND');
 
 
+//Game Text Entry Box
+var guessEntry = document.getElementById('Input').value;
+
+function wordGuess () {
+  var guessentry = document.getElementById('guessText').value;
+  docmuent.getElementById('entry').innterHTML = guessentry;
+}
