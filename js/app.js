8f0a6a0a354b01081cf5f00d2ead5e2fda238a99
Tyler Johnson <tyler@csdconsultants.us>
2021-03-06 09:19:43 -0800

    added score, allScores, username global variables and array. Added start of Leaderboard constructor to generate username and score into allScores array. Added start of renderLeaderboardHeader function to render table

--- js/app.js
+++ js/app.js
@@ -19,11 +19,14 @@ let points = 0;
 let seconds = 10;
 let totalGuess = 0;
 let wrongGuess= 10;
+let score = 0;
+let allScores = [];
+let username = 'tyler';
 // button ID - play
 
 const wordList = [ 'mask', 'pizza', 'covid', 'pliers', 'camera', 'vacuum',
   'pizazz', 'library', 'channel', 'vaccine', 'suburban', 'cemetery', 'calendar', 'separate', 'misspell', 'argument', 'assuming', 'definite', 'positive', 'negative', 'dachshund', 'necessary', 'possession', 'supposedly', 'quarantine', 'obstinance', 'millennium', 'processing', 'sovereignty', 'accommodate', 'fluorescent', 'mischievous', 'accidentally', 'questionnaire', 'pronunciation', 'capitalization' ];
-console.log(wordList);
+//console.log(wordList);
 
 function countdown() {
   points = 0;
@@ -37,3 +40,27 @@ function countdown() {
   }
   );
 }
+
+//constructor to generate allScores array
+//will then be saved to local storage
+function Leaderboard(username, score) {
+  this.username = username;
+  this.score = score;
+  allScores.push(this);
+}
+
+//saving allScores to local storage
+let stringifiedScore = JSON.stringify(allScores);
+localStorage.setItem('hyprtypr', stringifiedScore);
+
+//render leaderboard table to the DOM
+function renderLeaderboardHeader () {
+  let header = document.getElementById('leaderboard-table');
+  let th = document.createElement('th');
+  th.textContent = `${username} 10`;
+  header.appendChild(th);
+}
+
+// function renderLeaderboardScores
+new Leaderboard('tyler', 10);
+renderLeaderboardHeader();
