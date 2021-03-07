f624ef5a099c548259bbe7f17756b61e1c1d6f15
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-03-06 18:10:24 -0600

    timer done, Not restarting

--- js/app.js
+++ js/app.js
@@ -12,48 +12,72 @@ console.log('WWLND');
 // Scoring method
 // stretch goal -- Adding nested for loops seperate arrays
 
-let button = document.querySelector('button');
-let timeRemaining = document.querySelector('timeRemaining');
-let score = document.querySelector('score');
-let countdown = document.querySelector('time');
+let countdown = document.getElementById('time');
+let timeRemaining = document.getElementById('timeRemaining');
+let word = document.querySelector('word');
+let score = document.getElementById('score');
 let points = 0;
 let totalGuess = 0;
-let wrongGuess= 10;
+let wrongGuess = 10;
 let allScores = [];
-let username = 'tyler';
+let username = '';
 let timePerWord = 10;
-
+let startTimer = document.getElementById('start');
 
 
 const wordList = ['mask', 'pizza', 'covid', 'pliers', 'camera', 'vacuum',
-'pizazz', 'library', 'channel', 'vaccine', 'suburban', 'cemetery', 'calendar', 'separate', 'misspell', 'argument', 'assuming', 'definite', 'positive', 'negative', 'dachshund', 'necessary', 'possession', 'supposedly', 'quarantine', 'obstinance', 'millennium', 'processing', 'sovereignty', 'accommodate', 'fluorescent', 'mischievous', 'accidentally', 'questionnaire', 'pronunciation', 'capitalization'];
+  'pizazz', 'library', 'channel', 'vaccine', 'suburban', 'cemetery', 'calendar', 'separate', 'misspell', 'argument', 'assuming', 'definite', 'positive', 'negative', 'dachshund', 'necessary', 'possession', 'supposedly', 'quarantine', 'obstinance', 'millennium', 'processing', 'sovereignty', 'accommodate', 'fluorescent', 'mischievous', 'accidentally', 'questionnaire', 'pronunciation', 'capitalization'];
 // console.log(wordList);
 
 // Game Page
 
 
 function startGame() {
-  let timer = setInterval(function(){
-    button.disabled = true;
-    timePerWord --; 
+  let timer = setInterval(function () {
+    startTimer.disabled = false;
+    timePerWord--;
     countdown.innerHTML = timePerWord;
-    if(timePerWord === 0){
+    if (timePerWord === 0) {
+      console.log(points);
       score.innerHTML = '0';
       wordList.innerHTML = '';
-      button.innerHTML = false;
-      clearInterval(timer);
-      timePerWord = 10;
       timeRemaining.innerHTML = '10';
-      button.disabled = false;
+      startTimer.disabled = false;
     }
   }, 1000);
 }
 
 
+
+
+
+
 function wordGuess() {
   var guessEntry = document.getElementById('guessText').value;
   document.getElementById('entry').innerHTML = guessEntry;
+  console.log('clicked');
 }
+
+
+
+
+
+function countdownTimer(e) {
+  startGame();
+}
+
+
+startTimer.addEventListener("click", countdownTimer);
+
+
+
+
+
+// startGame();
+
+
+
+
 
 
 // LeaderBoard Page
@@ -63,14 +87,14 @@ function Leaderboard(username, score) {
   this.username = username;
   this.score = score;
   allScores.push(this);
-}
+};
 
 //saving allScores to local storage
 let stringifiedScore = JSON.stringify(allScores);
 localStorage.setItem('hyprtypr', stringifiedScore);
 
 //render leaderboard table to the DOM
-function renderLeaderboardHeader () {
+function renderLeaderboardHeader() {
   let header = document.getElementById('leaderboard-table');
   let th = document.createElement('th');
   th.textContent = `${username} 10`;
@@ -80,4 +104,3 @@ function renderLeaderboardHeader () {
 // function renderLeaderboardScores
 new Leaderboard('tyler', 10);
 renderLeaderboardHeader();
-
